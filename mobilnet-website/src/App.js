16c65a48c72b9e8af362d93aa5651b66d507861d
Ing. Nelson Gonzalez <nescalan@gmail.com>
2021-01-18 16:06:28 -0600

    Layout and 404:NotFound

--- mobilnet-website/src/App.js
+++ mobilnet-website/src/App.js
@@ -5,20 +5,25 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 // import Components
-import Navbar from "./components/Navbar";
 import Inicio from "./pages/Inicio";
+import Layout from "./components/Layout";
+import Navbar from "./components/Navbar";
 import Nosotros from "./pages/Nosotros";
+import NotFound from "./components/NotFound";
 import Productos from "./pages/Productos";
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Inicio} />
-        <Route exact path="/Navbar" component={Navbar} />
-        <Route exact path="/Nosotros" component={Nosotros} />
-        <Route exact path="/Productos" component={Productos} />
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Inicio} />
+          <Route exact path="/Navbar" component={Navbar} />
+          <Route exact path="/Nosotros" component={Nosotros} />
+          <Route exact path="/Productos" component={Productos} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
     </BrowserRouter>
   );
 }
---
+++ mobilnet-website/src/components/Layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// import Components
+import Navbar from "./Navbar";
+
+function Layout() {
+  return (
+    <React.Fragment>
+      <Navbar />
+    </React.Fragment>
+  );
+}
+
+export default Layout;
